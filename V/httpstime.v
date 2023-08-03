caf81cea8c4dc86a259b5f6083149c86ae3a5eb0
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-08-03 12:26:38 +0900

    Why is so slow

---
+++ V/httpstime.v
@@ -0,0 +1,9 @@
+import time
+import net.http
+
+resp := http.get('https://vlang.io/utc_now') or {
+    println('failed to fetch data from the server')
+    return
+}
+t := time.unix(resp.body.int())
+println(t.format())
