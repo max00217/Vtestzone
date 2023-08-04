5c70cd0f1988ae572f94217d95658270c6defc5f
max00217 <makpung00217@gmail.com>
2023-08-05 00:41:22 +0900

    nvim test

--- V/V.v
+++ V/V.v
@@ -1,12 +1,14 @@
 // import os
 
 fn main(){
-	langs := ["Lua", "V", "Python", "Kotlin", "JavaScript"]
-		printingsome(langs)
-	}
+	// langs := ["Lua", "V", "Python", "Kotlin", "JavaScript"]
+	// 	printingsome(langs)
+	// }
 
-	fn printingsome(arr []string){
-		for lang in arr {
-			println("${lang}")
-		}
+	// fn printingsome(arr []string){
+	// 	for lang in arr {
+	// 		println("${lang}")
+	// 	}
+	println(80*1.5)
+	println(20*1.5)
 }
