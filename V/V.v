6fb3136b2710df626cdaaa9df60eb490cf2f03f1
max00217 <makpung00217@gmail.com>
2023-08-04 14:43:41 +0900

    V

--- V/V.v
+++ V/V.v
@@ -1,12 +1,12 @@
 // import os
 
 fn main(){
-langs := ["Lua", "V", "Python", "Kotlin", "JavaScript"]
-printingsome(langs)
-}
-
-fn printingsome(arr []string){
-	for lang in arr {
-		println("${lang}")
+	langs := ["Lua", "V", "Python", "Kotlin", "JavaScript"]
+		printingsome(langs)
 	}
+
+	fn printingsome(arr []string){
+		for lang in arr {
+			println("${lang}")
+		}
 }
