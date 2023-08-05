3dc02cfdef3ae9de0561f0200b7451866b3e1e42
max00217 <makpung00217@gmail.com>
2023-08-06 02:01:50 +0900

    commit

---
+++ V/tests/pdf.v
@@ -0,0 +1,45 @@
+import pdf
+import os
+
+fn main() {
+	mut doc := pdf.Pdf{}
+	doc.init()
+
+	page_n := doc.create_page(pdf.Page_params{
+		format: "A4"
+		gen_content_obj: true
+		compress: false
+	})
+	mut page := &doc.page_list[page_n]
+	page.user_unit = pdf.mm_unit
+
+	mut fnt_params := pdf.Text_params{
+		font_size: 22.0
+		font_name: "Helvetica"
+		s_color: pdf.RGB{
+			r: 0
+			g: 0
+			b: 0
+		}
+		f_color: pdf.RGB{
+			r: 0
+			g: 0
+			b: 0
+		}
+	}
+
+	// Declare the base (Type1 font) we want use
+	if !doc.use_base_font(fnt_params.font_name) {
+		eprintln("ERROR: Font $fnt_params.font_name not available!")
+		return
+	}
+
+	// write the string
+	page.push_content(page.draw_base_text("Lel", 10, 10, fnt_params))
+
+	// render the PDF
+	txt := doc.render()!
+
+	// write it to a file
+	os.write_file_array("V.pdf", txt)!
+}
