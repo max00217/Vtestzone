f1177b788ed15db26a9d070d64046598ba5afd4a
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-08-12 00:48:18 +0900

    gitignore update

--- V/tests/clipboarder.v
+++ V/tests/clipboarder.v
@@ -3,10 +3,11 @@ import os
 
 fn main() {
 	println("what's on your clipboard?\n")
-	mut c := clipboard.new()
+	mut q := clipboard.new()
+	mut w := q.get_text()
 	for true{
 		if os.input("> Press Enter...") == ""{
-			println("\n==========\n\nThis is your clipboard: ${c.get_text()}\n\n==========\n")
+			println("\n==========\n\nThis is your clipboard: $w\n\n==========\n")
 		}
 	}	
 }
