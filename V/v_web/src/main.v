4ba377bf729c47b3bcabbd314e12de57ea8b8ebe
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-08-11 22:44:32 +0900

    vweb test

--- V/v_web/src/main.v
+++ V/v_web/src/main.v
@@ -10,6 +10,7 @@ struct App {
 struct Object {
 	title       string
 	description string
+	subtext string
 }
 
 fn main() {
@@ -43,5 +44,22 @@ pub fn (mut app App) page_home() vweb.Result {
 	]
 	// $vweb.html() in `<folder>_<name> vweb.Result ()` like this
 	// render the `<name>.html` in folder `./templates/<folder>`
+	return $vweb.html()
+}
+
+['/next']
+pub fn (mut app App) page_next() vweb.Result {
+	this_is_title := "Next V Page"
+	v_url := "https://www.youtube.com"
+
+	this_is := "V"
+
+	next_object := [
+		Object{
+			title: "Youtube"
+			subtext: "V로 웹 사이트 만들 건데 이거 한국어 지원합니까"
+		}
+	]
+
 	return $vweb.html()
 }
