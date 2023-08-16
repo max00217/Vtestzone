10eacafbc1ea0864ae6f9a6d2b0f9a747cb8ca24
max00217 <makpung00217@gmail.com>
2023-08-16 20:02:12 +0900

    Webreloader Complete

--- V/tests/WebReload.v
+++ V/tests/WebReload.v
@@ -2,17 +2,21 @@ import net.http
 
 fn main() {
     username := "max00217"
-    api_token := "a78785211daf50069f3ff1991d862de30bc4b9a5"
+    api_token := "6ac4304b6f0de3ac1630e7c13fc02501292847bc"
     domain_name := "max00217.pythonanywhere.com"
-    url := "https://www.pythonanywhere.com/api/v0/user/${username}/webapps/${domain_name}/reload/"
+    url := "https://www.pythonanywhere.com/api/v0/user/$username/webapps/$domain_name/reload/"
 
     mut header := http.new_header()
     header.add(.authorization, "Token $api_token")
 
-    response := http.head(url) or {
-        eprintln("Failed to reload web: ${err.msg()}")
-        return
+    // post 메소드 사용
+    config := http.FetchConfig{
+        method: .post,
+        url: url,
+        header: header
     }
+
+    response := http.fetch(config) !
 
     if response.status_code == 200 {
         println("Status code: ${response.status_code}\nPage Reloaded Successfully\nhttp://max00217.pythonanywhere.com/checking")
