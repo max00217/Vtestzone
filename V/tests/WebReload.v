866c021fb134aec5a6986edb529dc3b19e755b99
max00217 <makpung00217@gmail.com>
2023-08-16 18:07:12 +0900

    head

--- V/tests/WebReload.v
+++ V/tests/WebReload.v
@@ -1,27 +1,22 @@
 import net.http
 
-struct HeaderConfig {
-    key   http.CommonHeader
-    value string
-}
-
 fn main() {
     username := "max00217"
     api_token := "a78785211daf50069f3ff1991d862de30bc4b9a5"
     domain_name := "max00217.pythonanywhere.com"
     url := "https://www.pythonanywhere.com/api/v0/user/${username}/webapps/${domain_name}/reload/"
 
-    mut headers := http.new_header()
-    headers.add(.authorization, "Token $api_token")
+    mut header := http.new_header()
+    header.add(.authorization, "Token $api_token")
 
-    response := http.get(url) or {
-        eprintln("Failed to reload webapp: ${err.msg()}")
+    response := http.head(url) or {
+        eprintln("Failed to reload web: ${err.msg()}")
         return
     }
 
     if response.status_code == 200 {
-        println("Status code: ${response.status_code}\nPage Reloaded Successfully")
+        println("Status code: ${response.status_code}\nPage Reloaded Successfully\nhttp://max00217.pythonanywhere.com/checking")
     } else {
-        println("Unexpected Status code: ${response.status_code}\n$response.body")
+        println("Got Unexpected Status code: ${response.status_code}\n$response.body")
     }
 }
