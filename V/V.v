33117b88b1972954c958568c5f54d6e5203ec869
max00217 <makpung00217@gmail.com>
2023-08-24 17:05:11 +0900

    V

--- V/V.v
+++ V/V.v
@@ -1,5 +1,9 @@
 fn main() {
-	for i := 0; i < 10; i++ {
-		println('hello world')
-	}
+    mut sum := 0
+    for i in 1..11 {
+        if i % 2 == 0 {
+            sum += i
+        }
+    }
+    println("The sum of even numbers from 1 to 10 is: $sum")
 }
