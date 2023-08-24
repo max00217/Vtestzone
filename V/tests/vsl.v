5eaa64eb020619d6cf9efe51a413c48f280ce36a
max00217 <makpung00217@gmail.com>
2023-08-24 18:40:42 +0900

    vsl

---
+++ V/tests/vsl.v
@@ -0,0 +1,15 @@
+module main
+
+import vsl.plot
+
+mut plt := plot.new_plot()
+
+plt.add_trace(
+	trace_type: .bar
+	x_str: ['China', 'India', 'USA', 'Indonesia', 'Pakistan']
+	y: [1411778724.0, 1379217184, 331989449, 271350000, 225200000]
+)
+plt.set_layout(
+	title: 'Countries by population'
+)
+plt.show()!
