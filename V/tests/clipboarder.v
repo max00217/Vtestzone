f7b0fc8c081d7d511b277eb2d1f80d2ce8430448
max00217 <makpung00217@gmail.com>
2023-09-12 12:17:43 +0900

    Clipboarder v0.4

--- V/tests/clipboarder.v
+++ V/tests/clipboarder.v
@@ -3,12 +3,13 @@ import crayon
 import os
 
 fn main() {
-	println("what's on your clipboard? v0.3\n")
-	mut q := clipboard.new()
-	mut w := crayon.new(q.get_text()).light_magenta().str()
+	println(crayon.color("{green.bold.italic what's on your clipboard? v0.4}\n"))
 	for true{
-		if os.input("> Press Enter...") == ""{
-			println("\n==========\n\nThis is your clipboard:\n\n $w\n\n==========\n")
+		mut q := clipboard.new()
+		mut w := crayon.new(q.get_text()).light_magenta().bold().str()
+		if os.input("> Press Enter...\n(Press ctrl + c to End)") == ""{
+			println(crayon.color("\n{red ==========}\n\nThis is your clipboard:\n\n$w\n\n{red ==========}\n"))
 		}
+		q = clipboard.new()
 	}	
 }
