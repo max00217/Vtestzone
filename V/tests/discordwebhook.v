41a9afcc471ead47b6177589043fccd5f2904da1
max00217 <makpung00217@gmail.com>
2023-09-14 13:26:44 +0900

    즉럽다

--- V/tests/discordwebhook.v
+++ V/tests/discordwebhook.v
@@ -1,14 +1,19 @@
 import discordwebhook
+import os
 
 fn main() {   
     // Replace with your Discord webhook URL
-    webhook_url := "https://discord.com/api/webhooks/1151717371086655518/MaVaoSA7cgLq0G4ADVxtE9k72ALSml1mKFrnuDnt-dMxxrCE430YLRW21Ywyc5Dx9ZYx"
+    webhook_url := "https://discord.com/api/webhooks/1151734885283811378/gJftYlq4QoWUQfoGi_KfHxV6QubfywHsKxSHoKlEvHwWdfjyBkSBlP0a2bm3JaQiojR9"
 
     // Create a message with the content you want to send
-    message := discordwebhook.Message{ 
-        content: "컴파일해서 채팅용으로 만들어야지",
-    }
+	for true {
+		msg := os.input("")
 
-    resp := discordwebhook.send_message(webhook_url, message) !
-    println(resp)
+		message := discordwebhook.Message{ 
+			content: msg,
+		}
+
+		resp := discordwebhook.send_message(webhook_url, message) !
+		println(resp)
+	}
 }
