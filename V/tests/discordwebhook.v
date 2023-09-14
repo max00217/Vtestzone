0393690a5f367272efcb030514f038eb4226068a
max00217 <makpung00217@gmail.com>
2023-09-14 13:36:07 +0900

    wh

--- V/tests/discordwebhook.v
+++ V/tests/discordwebhook.v
@@ -3,11 +3,11 @@ import os
 
 fn main() {   
     // Replace with your Discord webhook URL
-    webhook_url := "https://discord.com/api/webhooks/1151734885283811378/gJftYlq4QoWUQfoGi_KfHxV6QubfywHsKxSHoKlEvHwWdfjyBkSBlP0a2bm3JaQiojR9"
+    webhook_url := "webhooked"
 
     // Create a message with the content you want to send
 	for true {
-		msg := os.input("")
+		msg := os.input("msg: ")
 
 		message := discordwebhook.Message{ 
 			content: msg,
