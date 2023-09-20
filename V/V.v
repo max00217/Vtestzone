50009f042dd596b300d97ce1059e97d781d2a429
max00217 <makpung00217@gmail.com>
2023-09-20 11:13:50 +0900

    What is none

--- V/V.v
+++ V/V.v
@@ -1,14 +1,5 @@
 fn main() {
-	mut day := ["M", "T", "W", "H", "F"]
-	mut time := ["l", "f", "d", "s", "t"]
-	mut room := ["1", "2", "3", "4"]
-
-	for i in 0..day.len {
-		for j in 0..time.len {
-			for k in 0..room.len {
-				println("${day[i]}${time[j]}${room[k]}")
-			}
-		}
-	}
+	a := false
+	println(a)
 }
 
