c82653cef8a21356303df0b90ec90d480a261df6
max00217 <makpung00217@gmail.com>
2023-09-25 11:10:47 +0900

    Bubblesort

---
+++ V/algorithm/bubblesort.v
@@ -0,0 +1,25 @@
+import rand
+
+fn main() {
+	mut arr := [
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!, 
+		rand.i64_in_range(1, 10)!,
+	]
+	println(arr)
+	for i in 1..arr.len - 1 {
+		for j in 0..arr.len - i -1 {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				println("Res...:${arr}")
+			}
+		}
+	}
+	println("Sort Result: ${arr}")
+}	
