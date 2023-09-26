47a0905eff9946fdc54e4ea4a7688fa7138cbbff
max00217 <makpung00217@gmail.com>
2023-09-26 11:07:42 +0900

    Copilot optimze

--- V/algorithm/sort_algorithms/bubblesort.v
+++ V/algorithm/sort_algorithms/bubblesort.v
@@ -23,3 +23,35 @@ fn main() {
 	}
 	println("Sort Result: ${arr}")
 }	
+
+// Copilot Optimized
+// import rand
+
+// fn main() {
+//     mut arr := [
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//         rand.i64_in_range(1, 10)!,
+//     ]
+//     println(arr)
+//     for i in 1..arr.len - 1 {
+//         mut swapped := false
+//         for j in 0..arr.len - i {
+//             if arr[j] > arr[j+1] {
+//                 arr[j], arr[j+1] = arr[j+1], arr[j]
+//                 swapped = true
+//                 println("Sorting...:${arr}")
+//             }
+//         }
+//         if !swapped {
+//             break
+//         }
+//     }
+//     println("Sort Result: ${arr}")
+// }
