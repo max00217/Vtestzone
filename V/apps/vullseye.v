9513644dd9453a4c9c0a40786e752143f9a514db
max00217 <makpung00217@gmail.com>
2023-09-27 11:32:30 +0900

    vullseye v0.2

--- V/apps/vullseye.v
+++ V/apps/vullseye.v
@@ -4,48 +4,62 @@ import os
 import crayon
 
 fn main() {
-	println(crayon.color("{green.bold.italic Vull's Eye v0.1.1}\n"))
+	println(crayon.color("{green.bold.italic Vull's Eye v0.2}\n"))
     mut userpoint := 0
-    for _ in 0..5 {
-        mut index := rand.intn(99) or { return }
+    for true {
+        for _ in 0..5 {
+            mut index := rand.intn(99) or { return }
 
-        mut line := '['
-        for j in 0..100 {
-            if j == index {
-                line += '◉'
-            } else {
-                line += ' '
+            mut line := '['
+            for j in 0..100 {
+                if j == index {
+                    line += '◉'
+                } else {
+                    line += ' '
+                }
             }
+            line += ']'
+            println(crayon.color("{red.bold ${line}}"))
+            userinput := os.input(crayon.color("{green.italic Guess the Number...: }")).int()
+            if userinput == index {
+                userpoint = userpoint + 100
+                println(crayon.color("{red.bold.italic \n+ 100pts!}"))
+
+            } else if math.abs(userinput - index) < 5 {
+                userpoint = userpoint + 95
+                println(crayon.color("{yellow.bold \n+ 95pts}"))
+            } else if math.abs(userinput - index) < 10 {
+                userpoint = userpoint + 90
+                println(crayon.color("{green.bold \n+ 90pts}"))
+
+            } else if math.abs(userinput - index) < 15 {
+                userpoint = userpoint + 80
+                println(crayon.color("{blue.bold \n+ 80pts}"))
+            } else if math.abs(userinput - index) < 20 {
+                userpoint = userpoint + 70
+                println(crayon.color("{cyan.bold \n+ 70pts}"))
+            } else if math.abs(userinput - index) < 25 {
+                userpoint = userpoint + 55
+                println(crayon.color("{white \n+ 55pts}"))
+            } else if math.abs(userinput - index) < 30 {
+                userpoint = userpoint + 45
+                println(crayon.color("{light_gray \n+ 45pts}"))
+            } else {
+                println(crayon.color("{dark_gray.italic \nThere are no points for you...}"))
+            }
+            println(crayon.color("{bold Your Score: ${userpoint}pts\n}"))
         }
-        line += ']'
-        println(crayon.color("{red.bold ${line}}"))
-        userinput := os.input(crayon.color("{green.italic Guess the Number...: }")).int()
-        if userinput == index {
-            userpoint = userpoint + 100
-            println(crayon.color("{red.bold.italic \n+ 100pts!}"))
-
-        } else if math.abs(userinput - index) < 5 {
-            userpoint = userpoint + 95
-            println(crayon.color("{yellow.bold \n+ 95pts}"))
-        } else if math.abs(userinput - index) < 10 {
-            userpoint = userpoint + 90
-            println(crayon.color("{green.bold \n+ 90pts}"))
-
-        } else if math.abs(userinput - index) < 15 {
-            userpoint = userpoint + 80
-            println(crayon.color("{blue.bold \n+ 80pts}"))
-        } else if math.abs(userinput - index) < 20 {
-            userpoint = userpoint + 70
-            println(crayon.color("{cyan.bold \n+ 70pts}"))
-        } else if math.abs(userinput - index) < 25 {
-            userpoint = userpoint + 55
-            println(crayon.color("{white \n+ 55pts}"))
-        } else if math.abs(userinput - index) < 30 {
-            userpoint = userpoint + 45
-            println(crayon.color("{light_gray \n+ 45pts}"))
+        mut continuesign := os.input(crayon.color("{red.italic Continue?(y/n): }"))
+        if continuesign == "y" || continuesign == "Y" {
+            userpoint = 0
+            continue
+            println("")
+        } else if continuesign == "n" || continuesign == "N" {
+            break
         } else {
-            println(crayon.color("{dark_gray.italic \nThere are no points for you...}"))
+            println(":(")
+            break
         }
-        println(crayon.color("{bold Your Score: ${userpoint}pts\n}"))
+
     }
 }
