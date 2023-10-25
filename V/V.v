c891e7073a355fb51d710ac729ca52b3d5f03582
max00217 <makpung00217@gmail.com>
2023-10-25 16:29:35 +0900

    BIGINT test

--- V/V.v
+++ V/V.v
@@ -1,8 +1,15 @@
-fn factorial(n int) int {
-    if n <= 1 {
-        return 1
-    }
-    return n * factorial(n - 1)
-}
+import math.big
+import math
 
-println(factorial(8))
+// fn factorial(n int) int {
+//     if n <= 1 {
+//         return 1
+//     }
+//     return n * factorial(n - 1)
+// }
+
+// println(factorial(8))
+
+fn main() {
+    println(big.integer_from_int(int(math.pow(10, 90))))
+}
