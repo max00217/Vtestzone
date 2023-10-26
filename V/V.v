963d767af3be695af8ecad9bb205e0b9dc3d0c93
max00217 <makpung00217@gmail.com>
2023-10-26 15:54:54 +0900

    Logic gate simulating

--- V/V.v
+++ V/V.v
@@ -1,15 +1,51 @@
-import math.big
-import math
+fn v_and(q int, w int) int {
+    if q == 1 && w == 1 {
+        return 1  
+    } else {
+        return 0
+    }
+}
 
-// fn factorial(n int) int {
-//     if n <= 1 {
-//         return 1
-//     }
-//     return n * factorial(n - 1)
-// }
+fn v_or(q int, w int) int {
+    if q == 1 || w == 1 {
+        return 1
+    } else {
+        return 0
+    }
+}
 
-// println(factorial(8))
+fn v_not(q int) int {
+    if q == 1 {
+        return 0
+    } else {
+        return 1
+    } 
+}
 
-fn main() {
-    println(big.integer_from_int(int(math.pow(10, 90))))
+fn v_nand(q int, w int) int {
+    if q == 0 && w == 0 {
+        return 1       
+    } else {
+        return 0
+    }
+}
+
+fn v_xor(q int, w int) int {
+    if q != w {
+        return 1       
+    } else {
+        return 0
+    }
+}
+
+fn v_xnor(q int, w int) int {
+    if q == w {
+        return 1       
+    } else {
+        return 0
+    }
+}
+
+fn v_buf(q int) int {
+    return q
 }
