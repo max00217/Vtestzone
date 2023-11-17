cffd4b1b716c09a3d426fc5868a72e7f642c2596
max00217 <makpung00217@gmail.com>
2023-11-17 12:31:55 +0900

    Refactor Vlog index.html and main.html

--- V/Vlog/vlog.v
+++ V/Vlog/vlog.v
@@ -1,31 +1,55 @@
-// blog.v
+//start this with ``v run .``
 module main
 
 import vweb
 import time
+import db.sqlite
 
 struct App {
 	vweb.Context
+pub mut:
+	db sqlite.DB
 }
 
 fn main() {
-	app := App{}
-	vweb.run(app, 8081)
+	mut app := App{
+		db: sqlite.connect(':memory:')!
+	}
+	sql app.db {
+		create table Article
+	}!
+
+	first_article := Article{
+		title: 'Hello, world!'
+		text: 'V is great.'
+	}
+
+	second_article := Article{
+		title: 'Second post.'
+		text: 'Hm... what should I write about?'
+	}
+
+	sql app.db {
+		insert first_article into Article
+		insert second_article into Article
+	}!
+	vweb.run(app, 1972)
 }
 
+["/time"]
 fn (mut app App) time() vweb.Result {
-	return app.text(time.now().format())
+  currunttime := time.now().format()
+  return $vweb.html()
 }
 
 ["/index"]
-pub fn (mut app App) index() vweb.Result {
-	message := "Vweb 메인 페이지"
-	msg2 := "이제 DB를 넣어야 하는"
+pub fn (app &App) index() vweb.Result {
+	articles := app.find_all_articles()
 	return $vweb.html()
 }
 
 ["/main"]
-pub fn (mut app App) main() vweb.Result {
+pub fn (mut app App) main1972() vweb.Result {
 	msg1 := "Vweb 테스트"
 	msg2 := "다른 페이지로 넘어가는 법"
 	return $vweb.html()
